978d65ffcfd0971a6af6d45186b7b173070c9e25
Mu-Chu Lee <mlee8@meta.com>
2023-11-15 12:27:52 -0800

    [TEST]
    ghstack-source-id: 18090738bc624ab873dea07cdb1850ad653681d8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113243

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -4153,7 +4153,7 @@ class IndexPutFallback(ExternKernel):
 class DeviceCopy(ExternKernelOut):
     @classmethod
     def create(cls, x, device):
-        if not x.is_extern() and all(
+        if not x.is_extern() and not config.split_const_graph and all(
             (r.name in V.graph.constants and isinstance(r, dependencies.MemoryDep))
             for r in x.get_reads()
         ):
